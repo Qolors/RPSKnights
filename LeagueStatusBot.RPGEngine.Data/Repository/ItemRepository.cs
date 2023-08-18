1667806742deca69c051b6d056b3b94c44aa5151
Christopher McGowen <chrismcgowen31@gmail.com>
2023-08-17 23:40:58 -0400

    Repository & DTOs

---
+++ LeagueStatusBot.RPGEngine.Data/Repository/ItemRepository.cs
@@ -0,0 +1,67 @@
+﻿using LeagueStatusBot.RPGEngine.Data.Contexts;
+using LeagueStatusBot.RPGEngine.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStatusBot.RPGEngine.Data.Repository
+{
+    public class ItemRepository
+    {
+        private readonly GameDbContext _context;
+
+        public ItemRepository(GameDbContext context)
+        {
+            _context = context;
+        }
+
+        public ItemEntity AddItem(ItemEntity item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return item;
+        }
+
+        public bool DeleteItem(int itemId)
+        {
+            var item = _context.Items.FirstOrDefault(i => i.ItemId == itemId);
+            if (item == null)
+            {
+                return false; // Item not found
+            }
+
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public ItemEntity GenerateRandomItem()
+        {
+            var randomEffectId = GetRandomEffectId();
+
+            var newItem = new ItemEntity
+            {
+                ItemName = $"Generated Item {Guid.NewGuid()}",
+                ItemType = /* Assign appropriate ItemType */,
+                Rarity = /* Assign appropriate ItemRarity */,
+                ItemEffect = randomEffectId
+            };
+
+            return AddItem(newItem);
+        }
+
+        private int GetRandomEffectId()
+        {
+            var effects = _context.EffectEntities.ToList();
+            var random = new Random();
+            var randomEffect = effects[random.Next(effects.Count)];
+
+            return randomEffect.EffectId;
+        }
+    }
+
+}
---
+++ LeagueStatusBot.RPGEngine.Data/Repository/PlayerRepository.cs
@@ -0,0 +1,153 @@
+﻿using LeagueStatusBot.RPGEngine.Data.Contexts;
+using LeagueStatusBot.RPGEngine.Data.Entities;
+using LeagueStatusBot.RPGEngine.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStatusBot.RPGEngine.Data.Repository
+{
+    public class PlayerRepository
+    {
+        private GameDbContext dbContext;
+        public PlayerRepository(GameDbContext gameDbContext)
+        {
+            this.dbContext = gameDbContext;
+        }
+
+        public bool Exists(ulong key)
+        {
+            return dbContext
+                .Set<BeingEntity>()
+                .Find(key) != null;
+        }
+
+        public BeingEntity? GetBeingByDiscordId(ulong key)
+        {
+            return dbContext
+                .Beings
+                .SingleOrDefault(b => b.DiscordId == key);
+        }
+
+        public void UpdatePlayerStats(ulong discordId, int strength, int luck, int endurance, int charisma, int intelligence, int agility)
+        {
+            var player = GetBeingByDiscordId(discordId);
+            if (player == null)
+            {
+                throw new Exception($"Player with Discord ID {discordId} not found.");
+            }
+
+            // Update the stats
+            player.Strength = strength;
+            player.Luck = luck;
+            player.Endurance = endurance;
+            player.Charisma = charisma;
+            player.Intelligence = intelligence;
+            player.Agility = agility;
+
+            dbContext.SaveChanges();
+        }
+
+        public bool DeletePlayerByDiscordId(ulong discordId)
+        {
+            var player = dbContext.Beings.FirstOrDefault(p => p.DiscordId == discordId);
+            if (player == null)
+            {
+                return false; // Player not found
+            }
+
+            dbContext.Beings.Remove(player);
+            dbContext.SaveChanges();
+
+            return true; // Successfully deleted
+        }
+
+        public BeingEntity? UpdateEquipment(ulong discordId, ItemType type, int itemId)
+        {
+            var player = dbContext.Beings.FirstOrDefault(p => p.DiscordId == discordId);
+            if (player == null)
+            {
+                return null; // Player not found
+            }
+
+            switch (type)
+            {
+                case ItemType.Helm:
+                    player.Helm = itemId;
+                    break;
+                case ItemType.Chest:
+                    player.Chest = itemId;
+                    break;
+                case ItemType.Legs:
+                    player.Legs = itemId;
+                    break;
+                case ItemType.Boots:
+                    player.Boots = itemId;
+                    break;
+                case ItemType.Gloves:
+                    player.Gloves = itemId;
+                    break;
+                case ItemType.Weapon:
+                    player.Weapon = itemId;
+                    break;
+                default:
+                    throw new ArgumentException($"Unsupported ItemType: {type}");
+            }
+
+            dbContext.SaveChanges();
+
+            return player;
+        }
+
+        public BeingEntity? AddToInventory(ulong discordId, int itemId)
+        {
+            var player = dbContext.Beings.FirstOrDefault(p => p.DiscordId == discordId);
+            if (player == null)
+            {
+                return null; // Player not found
+            }
+
+            if (player.Inventory == null)
+            {
+                player.Inventory = new List<int>();
+            }
+
+            if (!player.Inventory.Contains(itemId))
+            {
+                player.Inventory.Add(itemId);
+            }
+
+            dbContext.SaveChanges();
+
+            return player;
+        }
+
+        public BeingEntity? RemoveFromInventory(ulong discordId, int itemId)
+        {
+            var player = dbContext.Beings.FirstOrDefault(p => p.DiscordId == discordId);
+            if (player == null)
+            {
+                return null; // Player not found
+            }
+
+            if (player.Inventory == null)
+            {
+                return player; // Nothing to remove
+            }
+
+            if (player.Inventory.Contains(itemId))
+            {
+                player.Inventory.Remove(itemId);
+            }
+
+            dbContext.SaveChanges();
+
+            return player;
+        }
+
+
+    }
+}
