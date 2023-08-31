4aa0bb93fef21d6e9e3c3f2b1e08fbd8f2653985
Qolors <cmickyg@gmail.com>
2023-08-31 09:32:30 -0400

    DbContext Set

---
+++ LeagueStatusBot.RPGEngine.Data/Repository/MonsterRepository.cs
@@ -0,0 +1,21 @@
+﻿using LeagueStatusBot.RPGEngine.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStatusBot.RPGEngine.Data.Repository
+{
+    public class MonsterRepository
+    {
+        private GameDbContext context;
+        public MonsterRepository(GameDbContext context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
